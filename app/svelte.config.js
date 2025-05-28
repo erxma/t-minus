1672d43c3d94393db75da04ad16e8013f62cd8e8
erxma <47842317+erxma@users.noreply.github.com>
2025-05-28 05:30:11 -0400

    Add option to build for Netlify

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,5 +1,18 @@
-import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const DEPLOY_ADAPTER = process.env.DEPLOY_ADAPTER ?? "node";
+
+let adapter;
+switch (DEPLOY_ADAPTER) {
+	case "node":
+		adapter = (await import("@sveltejs/adapter-node")).default;
+		break;
+	case "netlify":
+		adapter = (await import("@sveltejs/adapter-netlify")).default;
+		break;
+	default:
+		throw new Error(`Unexpected deploy adapter '${DEPLOY_ADAPTER}'.`);
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
