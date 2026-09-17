857c5d4a55d03d05c5be8005c8cae5c23eab88fd
erxma <47842317+erxma@users.noreply.github.com>
2026-09-16 23:24:13 -0400

    Upgrade to Vitest 5 and fix preexisting vite.config issues

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,13 +1,12 @@
 import { svelteTesting } from "@testing-library/svelte/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     plugins: [sveltekit()],
     test: {
-        workspace: [
+        projects: [
             {
-                extends: "./vite.config.ts",
                 plugins: [svelteTesting()],
                 test: {
                     name: "client",
@@ -19,7 +18,6 @@ export default defineConfig({
                 },
             },
             {
-                extends: "./vite.config.ts",
                 test: {
                     name: "server",
                     environment: "node",
